4e949738cf68e222fadc5d0d49bbfc1728aee4e8
Vikas Kumar <44707673+vkn84527@users.noreply.github.com>
2021-05-11 22:48:51 +0530

    Add files via upload

---
+++ jshtml/Export/import/index.js
@@ -0,0 +1,5 @@
+const {add,sub,mul,name}= require('./oper')
+console.log(name)
+console.log(add(5,5))
+console.log(sub(25,5))
+console.log(mul(10,50))
---
+++ jshtml/demo1/demo.js
@@ -0,0 +1,6 @@
+//document.write("<h1>JS Embedded2</h1>");
+var c=15;
+document.write("<h1>cikfgh</h1>",c);
+n='vikas';
+alert(n);
+
